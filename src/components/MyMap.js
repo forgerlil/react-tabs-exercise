4c6ba304f867ce7e18651e54940fc63ea66d87ac
Lilian Forger <lilianforger@gmail.com>
2022-04-14 13:44:18 +0200

    Dependencies working correctly after adding leaflet beta version

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const MyMap = () => {
   const position = [51.505, -0.09]
